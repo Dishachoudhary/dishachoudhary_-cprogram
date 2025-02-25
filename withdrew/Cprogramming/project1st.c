2f49c628848d88fd741be4e10b60f60854a98a2a
Indixpert_sainisandeep1947 <sandeepsaini20251930@gamil.com>
2025-02-24 20:42:39 -0800

    change code

---
+++ withdrew/Cprogramming/project1st.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void createaccount();
+void deposit();
+void withdraw();
+void checkbalance();
+void transactionhistory();
+void exitsystem();
+
+float balance=0.0;
+
+int main(){
+    int choice;
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
--- withdrew/withdrew.c
+++ withdrew/withdrew.c
@@ -9,10 +9,13 @@ float withdrawamount;
 
 printf("current balance=%.2f\n", balance);
 printf("enter amount to withdraw:- ");
-scanf("%f", &withdrawamount );
+scanf("%f", &withdrawamount ); 
+if(withdrawamount == 500|| withdrawamount == 1000 ||withdrawamount == 1500 || withdrawamount == 2000 ){
 if(balance >= withdrawamount){
     balance = balance - withdrawamount;
     printf("withdraw successfully! new balance= %.2f\n", balance);
+}
+
 }
   else{
     printf("insufficient balance:\n");
